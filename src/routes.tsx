16a39400aa3b1ca2c25a07d58ba95626b0342e15
Anderson Matos <ams.bns2021@gmail.com>
2022-12-01 12:09:13 -0300

    excluir rota inexistente

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Boleto from './pages/Boleto';
-import HomePage from './pages/HomePage';
 import Cliente from './pages/Cliente';
 import Cobranca from './pages/Cobranca';
-import CollapsibleTable from './pages/PaginaTeste';
-import Cabecalho from './components/Cabecalho';
-import ListaDeDados from './pages/ListaDeDados';
+import HomePage from './pages/HomePage';
 
 export default function Rotas() {
     // const [nomeCliente, setNomeCliente] = useState("")
@@ -18,7 +16,7 @@ export default function Rotas() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/todosClientes" element={<ListaDeDados />} />
+                
                     <Route path="/cliente" element={<Cliente />} />
                     <Route path='/boleto' element={<Boleto />} />
                     <Route path='/cobranca' element={<Cobranca />} />
